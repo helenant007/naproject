4db647bb4fff410af46b704a2bafe2a41c2666c8
Helena Natanael <helena.natanael@tokopedia.com>
2018-07-12 08:41:18 +0700

    format date

--- model/user.go
+++ model/user.go
@@ -2,24 +2,28 @@ package model
 
 import (
 	"log"
+	"strings"
 	"time"
 
 	"github.com/helenant007/naproject/utils/database"
 )
 
 type User struct {
-	ID          string
-	Name        string
-	MSISDN      string
-	Email       string
-	BirthDate   time.Time
-	CreatedTime time.Time
-	UpdateTime  time.Time
-	UserAge     int
+	ID                string
+	Name              string
+	MSISDN            string
+	Email             string
+	BirthDate         time.Time
+	BirthDateString   string
+	CreatedTime       time.Time
+	CreatedTimeString string
+	UpdateTime        time.Time
+	UpdateTimeString  string
+	UserAge           int
 }
 
 // GetUsers : to get User data from DB
-func GetUsers() ([]*User, error) {
+func GetUsers(nameWhere string) ([]*User, error) {
 	result := []*User{}
 	db := database.GetDB()
 
@@ -35,7 +39,17 @@ func GetUsers() ([]*User, error) {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		if nameWhere != "" && !strings.Contains(user.Name, nameWhere) {
+			continue
+		}
+
 		user.UserAge = int(time.Since(user.BirthDate).Hours() / (float64(24 * 365))) // count age manually
+		dateFormat := "2006/01/02 03:04:05"
+		user.BirthDateString = user.BirthDate.Format(dateFormat)
+		user.CreatedTimeString = user.CreatedTime.Format(dateFormat)
+		user.UpdateTimeString = user.UpdateTime.Format(dateFormat)
+
 		result = append(result, user)
 	}
 
